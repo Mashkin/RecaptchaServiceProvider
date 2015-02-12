a4a0820dd7429c1e649bc82343f0afa784ac7629
Mashkin <mashkin@web.de>
2015-02-12 20:23:14 +0100

    Create RecaptchaServiceProvider.php

---
+++ src/Mashkin/RecaptchaServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of RecaptchaServiceProvider
+ *
+ * Copyright (c) 2015 Mashkin
+ * For license information please read the LICENSE file provided with this source code
+ */
+
+namespace Mashkin;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class RecaptchaServiceProvider implements ServiceProviderInterface
+{
+
+    public function register (Application $app)
+    {
+        $app['recaptcha.language'] = 'en';
+        $app['recaptcha.streamContext'] = null;
+
+        $app['recaptcha'] = $app->share(function () use ($app) {
+            return new Recaptcha($app['recaptcha.sitekey'], $app['recaptcha.secret'],
+                $app['recaptcha.language'], $app['recaptcha.streamContext']);
+        });
+    }
+
+    public function boot (Application $app)
+    {
+
+    }
+
+}
